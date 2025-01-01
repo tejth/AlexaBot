dc92e6313bf1007d9edf8c302e37d62746fbd2c1
tejth <ptejendra91@gmail.com>
2025-01-01 22:01:32 +0530

    creating a takecommand function to take user input using voice with the help of speechRecognition and PyAudio library

--- engine/command.py
+++ engine/command.py
@@ -1,4 +1,5 @@
 import pyttsx3
+import speech_recognition as sr
 
 def speak(text):
     engine = pyttsx3.init('sapi5')
@@ -7,4 +8,22 @@ def speak(text):
     engine.setProperty('rate',174)
     engine.say(text)
     engine.runAndWait()
+
+def takecommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:  # Add parentheses to correctly instantiate the Microphone
+        print("Listening...")
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source)
+        
+        audio = r.listen(source, 10, 6)
+        
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language='en-in')
+        print(f"user said: {query}")  # Corrected the f-string formatting
+    except Exception as e:
+        print(f"Error: {e}")  # Optional: prints the error if there's an exception
+        return ""
     
+    return query.lower()
