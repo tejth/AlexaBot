420deefef82ec72e4a1d4a7e1384600e2a31cd03
tejth <ptejendra91@gmail.com>
2025-01-02 18:58:47 +0530

    added open functionality to open apps such as notepad etc..

--- engine/command.py
+++ engine/command.py
@@ -1,17 +1,18 @@
 import pyttsx3
 import speech_recognition as sr
 import eel
+import time
 
 def speak(text):
     engine = pyttsx3.init('sapi5')
     voices = engine.getProperty('voices')
     engine.setProperty('voice',voices[1].id)
     engine.setProperty('rate',174)
+    eel.DisplayMessage(text)
     engine.say(text)
     engine.runAndWait()
 
 
-@eel.expose
 def takecommand():
     r = sr.Recognizer()
     with sr.Microphone() as source:  # Add parentheses to correctly instantiate the Microphone
@@ -28,14 +29,24 @@ def takecommand():
         query = r.recognize_google(audio, language='en-in')
         print(f"user said: {query}")  # Corrected the f-string formatting
         eel.DisplayMessage(query);
-        speak(query)
-        eel.ShowHood()
+        time.sleep(2)
     except Exception as e:
         print(f"Error: {e}")  # Optional: prints the error if there's an exception
         return ""
     
     return query.lower()
+
+@eel.expose
+def allCommands():
    
-   
+   query = takecommand()
+   print(query)   
+   if "open" in query:
+       from engine.features import openCommand
+       openCommand(query)
+   else:
+       print("not run")
+   eel.ShowHood()
+       
    
     
--- engine/features.py
+++ engine/features.py
@@ -1,5 +1,8 @@
+import os
 from playsound import playsound
 import eel
+from engine.config import ASSISTANT_NAME
+from engine.command import speak
 
 
 #playing assistant sound function
@@ -7,3 +10,15 @@ import eel
 def playAssistantSound():
     music_dir = "www\\assets\\audio\\start_sound.mp3"
     playsound(music_dir)
+    
+
+def openCommand(query):
+    query = query.replace(ASSISTANT_NAME,"")
+    query = query.replace("open","")
+    query.lower()
+    
+    if query!="":
+        speak("Opening "+query)
+        os.system("start "+query)
+    else:
+        speak("not found")
