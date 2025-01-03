e4ababb8610b15efb7567d4857ae6ea7475258e8
tejth <ptejendra91@gmail.com>
2025-01-03 20:53:39 +0530

    created button to implement chat feature

--- www/main.js
+++ www/main.js
@@ -53,4 +53,16 @@ $(document).ready(function () {
     }
   }
   document.addEventListener("keyup", doc_keyUp, false);
+
+  // to play assisatnt
+  function PlayAssistant(message) {
+    if (message != "") {
+      $("#Oval").attr("hidden", true);
+      $("#SiriWave").attr("hidden", false);
+      eel.allCommands(message);
+      $("#chatbox").val("");
+      $("#MicBtn").attr("hidden", false);
+      $("#SendBtn").attr("hidden", true);
+    }
+  }
 });
