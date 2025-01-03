744c165ea3d9ff21d0c7a2dd4e4f10f8e42e0a8c
tejth <ptejendra91@gmail.com>
2025-01-03 13:01:39 +0530

    hot word detection implementation using pvpporcupine library version 1.9.6 and multithreading

--- engine/features.py
+++ engine/features.py
@@ -1,12 +1,18 @@
 import os
+import struct
+import time
 from playsound import playsound
 import eel
+import pvporcupine
+import pyaudio
 from engine.config import ASSISTANT_NAME
 from engine.command import speak
 import pywhatkit as kit 
 import re
 import webbrowser
 import sqlite3
+
+from engine.helper import extract_yt_term
 
 
 conn = sqlite3.connect("alexa.db")
@@ -59,11 +65,44 @@ def PlayYoutube(query):
     search_term = extract_yt_term(query)
     speak("Playing "+search_term+" on YouTube")
     kit.playonyt(search_term)
+    
+def hotword():
+    porcupine=None
+    paud=None
+    audio_stream=None
+    try:
+       
+        # pre trained keywords    
+        porcupine=pvporcupine.create(keywords=["jarvis","alexa"]) 
+        paud=pyaudio.PyAudio()
+        audio_stream=paud.open(rate=porcupine.sample_rate,channels=1,format=pyaudio.paInt16,input=True,frames_per_buffer=porcupine.frame_length)
+        
+        # loop for streaming
+        while True:
+            keyword=audio_stream.read(porcupine.frame_length)
+            keyword=struct.unpack_from("h"*porcupine.frame_length,keyword)
 
-def extract_yt_term(command):
-    # Define a regular expression pattern to capture the song name
-    pattern = r'play\s+(.*?)\s+on\s+youtube'
-    # Use re.search to find the match in the command
-    match = re.search(pattern, command, re.IGNORECASE)
-    # If a match is found, return the extracted song name; otherwise, return None
-    return match.group(1) if match else None
+            # processing keyword comes from mic 
+            keyword_index=porcupine.process(keyword)
+
+            # checking first keyword detetcted for not
+            if keyword_index>=0:
+                print("hotword detected")
+
+                # pressing shorcut key win+j
+                import pyautogui as autogui
+                autogui.keyDown("win")
+                autogui.press("j")
+                time.sleep(2)
+                autogui.keyUp("win")
+                
+    except:
+        if porcupine is not None:
+            porcupine.delete()
+        if audio_stream is not None:
+            audio_stream.close()
+        if paud is not None:
+            paud.terminate()
+
+
+
--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import eel
 from engine.features import *
 from engine.command import *
 
-eel.init("www")
-playAssistantSound()
+def start():
+    
+    eel.init("www")
 
-os.system('start msedge.exe --app=""http://localhost:8000/index.html')
+    playAssistantSound()
 
-eel.start('index.html', mode=None, host='localhost', block=True)
+    os.system('start msedge.exe --app=""http://localhost:8000/index.html')
+
+    eel.start('index.html', mode=None, host='localhost', block=True)
 
