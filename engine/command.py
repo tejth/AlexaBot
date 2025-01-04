ae5a3b0bc6fb69a07f4ecd14582724f3f15236eb
tejth <ptejendra91@gmail.com>
2025-01-04 12:55:50 +0530

    implemented functions to automate calling and sending messages via phone using adb

--- engine/command.py
+++ engine/command.py
@@ -40,45 +40,58 @@ def takecommand():
 
 @eel.expose
 def allCommands(message=1):
-    
+
     if message == 1:
         query = takecommand()
-        print(query)  
-        eel.senderText(query) 
+        print(query)
+        eel.senderText(query)
     else:
         query = message
-        eel.senderText(query) 
-    
+        eel.senderText(query)
     try:
+
         if "open" in query:
             from engine.features import openCommand
             openCommand(query)
         elif "on youtube" in query:
             from engine.features import PlayYoutube
             PlayYoutube(query)
+        
         elif "send message" in query or "phone call" in query or "video call" in query:
-            from engine.features import findContact, whatsApp
-            message = ""
+            from engine.features import findContact, whatsApp, makeCall, sendMessage
             contact_no, name = findContact(query)
             if(contact_no != 0):
+                speak("Which mode you want to use whatsapp or mobile")
+                preferance = takecommand()
+                print(preferance)
 
-                if "send message" in query:
-                    message = 'message'
-                    speak("what message to send")
-                    query = takecommand()
+                if "mobile" in preferance:
+                    if "send message" in query or "send sms" in query: 
+                        speak("what message to send")
+                        message = takecommand()
+                        sendMessage(message, contact_no, name)
+                    elif "phone call" in query:
+                        makeCall(name, contact_no)
+                    else:
+                        speak("please try again")
+                elif "whatsapp" in preferance:
+                    message = ""
+                    if "send message" in query:
+                        message = 'message'
+                        speak("what message to send")
+                        query = takecommand()
+                                        
+                    elif "phone call" in query:
+                        message = 'call'
+                    else:
+                        message = 'video call'
+                                        
+                    whatsApp(contact_no, query, message, name)
 
-                elif "phone call" in query:
-                    message = 'call'
-                else:
-                    message = 'video call'
-
-                whatsApp(contact_no, query, message, name)
         else:
             from engine.features import chatBot
             chatBot(query)
     except:
         print("error")
-    eel.ShowHood()
-       
-   
     
+    eel.ShowHood()
